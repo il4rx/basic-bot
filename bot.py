d8fe05cb35330514e68f28ce76fa0b746cacd274
Zee. <77794245+LamePuppeteer@users.noreply.github.com>
2024-12-08 16:23:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,24 +5,16 @@ import platform
 import time, datetime
 from time import sleep
 
-
-# Start
 intents = discord.Intents().all()
 client = discord.Client(intents=discord.Intents.default())
 bot = commands.Bot(command_prefix="!!", help_command=None, intents=intents)
 
-# login vanity
 @bot.event
 async def on_ready():
     print("Logged as {0.user}".format(bot))
     print(platform.node() + " Joined as admin");
-
-    # bot presence
     await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name="Activity goes here"))
-
-# command
-# you should using prefix with @bot.command or using nonType Message as client.event
-# im guide with prefix and nonType message
+    
 helpMessage = """
 `help` - return the help message
 `test` - new begining
@@ -39,15 +31,12 @@ async def embed(ctx):
     embed = discord.Embed(title="This is the embed message", description="Any message has provided in here", color=discord.Colour.random())
     await ctx.send(embed=embed)
 
-# bot list command / help function 
-
 @bot.command()
 async def help(ctx):
   embed = discord.Embed(title=f"{ctx.author} - Help Command", description="This is the available course :", color=discord.Colour.random())
   embed.add_field(name="Commands :", value=helpMessage)
   await ctx.send(embed=embed)
 
-# say command by using args argument
 @bot.command()
 async def say(ctx, messages):
     await ctx.send(f"{ctx.author.mention} has typing --> {messages}")
@@ -60,10 +49,9 @@ async def purge(ctx, amount : int):
     await ctx.send(f"{ctx.author} Has purging this channel!")
     await ctx.channel.purge(limit=amount)
  
-# if you dont have the permissions
 @purge.error
 async def clear_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("You cant do that!")
-# connect your bot
+
 bot.run("Your bot token")
